710a97b7936ab9e82b1a10690d2317d50100e18c
autodocs <autodocs>
2018-12-19 12:04:24 +0000

    build based on bec6ced

---
+++ dev/search_index.js
@@ -0,0 +1,83 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#RadiationDetectorSignals.jl-1",
+    "page": "Home",
+    "title": "RadiationDetectorSignals.jl",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "api/#",
+    "page": "API",
+    "title": "API",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "api/#API-1",
+    "page": "API",
+    "title": "API",
+    "category": "section",
+    "text": "DocTestSetup  = quote\n    using RadiationDetectorSignals\nend"
+},
+
+{
+    "location": "api/#Types-1",
+    "page": "API",
+    "title": "Types",
+    "category": "section",
+    "text": "Order = [:type]"
+},
+
+{
+    "location": "api/#Functions-1",
+    "page": "API",
+    "title": "Functions",
+    "category": "section",
+    "text": "Order = [:function]"
+},
+
+{
+    "location": "api/#RadiationDetectorSignals.ChannelWaveform",
+    "page": "API",
+    "title": "RadiationDetectorSignals.ChannelWaveform",
+    "category": "type",
+    "text": "ChannelWaveform\n\nFields:\n\nevtno::Integer: channel-event number\nchno::Integer: channel number\nsamplerate::RealQuantity: sample rate\n``\n\n\n\n\n\n"
+},
+
+{
+    "location": "api/#Documentation-1",
+    "page": "API",
+    "title": "Documentation",
+    "category": "section",
+    "text": "Modules = [RadiationDetectorSignals]\nOrder = [:type, :function]"
+},
+
+{
+    "location": "LICENSE/#",
+    "page": "LICENSE",
+    "title": "LICENSE",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "LICENSE/#LICENSE-1",
+    "page": "LICENSE",
+    "title": "LICENSE",
+    "category": "section",
+    "text": "using Markdown\nMarkdown.parse_file(joinpath(@__DIR__, \"..\", \"..\", \"LICENSE.md\"))"
+},
+
+]}
